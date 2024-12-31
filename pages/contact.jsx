bdc990c8ddc58faff6602b62eecec95ed4b7bc4a
Joshua Carroll <carrolljosh1976@gmail.com>
2024-12-30 20:23:37 -0500

    added api folder and contact api route inside that api folder

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,7 @@
-import ContactPage from '../components/ContactPage';
+import ContactForm from '../components/contact/contact-form';
 
 export default function ContactPage() {
     return (
-        <ContactPage />
+        <ContactForm />
     )
 }
